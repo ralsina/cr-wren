f3917288208acee450100769f0823dfda5669837
Roberto Alsina <roberto.alsina@gmail.com>
2024-06-17 17:55:31 -0300

    Support errorFn

--- src/cr-wren.cr
+++ src/cr-wren.cr
@@ -6,7 +6,13 @@ lib Wren
   alias WrenBindForeignMethodFn = Void*
   alias WrenBindForeignClassFn = Void*
   alias WrenWriteFn = Proc(WrenVM, LibC::Char*, Nil)
-  alias WrenErrorFn = Void*
+
+  enum WrenErrorType
+    WREN_ERROR_COMPILE
+    WREN_ERROR_RUNTIME
+    WREN_ERROR_STACK_TRACE
+  end
+  alias WrenErrorFn = Proc(WrenVM, WrenErrorType, LibC::Char*, LibC::Int32T, LibC::Char*, Nil)
 
   struct WrenConfiguration
     reallocateFn : WrenReallocateFn
@@ -41,10 +47,13 @@ module Cr::Wren
 end
 
 config = Wren::WrenConfiguration.new
-print = ->(_vm : Wren::WrenVM, text : LibC::Char*) : Nil {puts String.new(text)}
 
 Wren.wrenInitConfiguration(pointerof(config))
-config.writeFn = print
+config.writeFn = ->(_vm : Wren::WrenVM, text : LibC::Char*) : Nil { puts String.new(text) }
+config.errorFn = ->(_vm : Wren::WrenVM, type : Wren::WrenErrorType, _module : LibC::Char*, line : LibC::Int32T, message : LibC::Char*) : Nil {
+  puts "Error: #{type} in module #{String.new(_module)} line: #{line}"
+  puts "Message: #{String.new(message)}"
+}
 
 vm = Wren.wrenNewVM(pointerof(config))
 result = Wren.wrenInterpret(vm, "main", "System.print(\"Hello, World!\")")
