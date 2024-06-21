f56c6076deb2196f4f6000064cb9cb175c00a4c7
Roberto Alsina <roberto.alsina@gmail.com>
2024-06-21 14:20:50 -0300

    Docs, example

--- examples/main.cr
+++ examples/main.cr
@@ -1,40 +1,4 @@
-# cr-wren
-
-This is a binding to embed the [Wren language](https://wren.io/) into
-Crystal and call Wren code from Crystal and viceversa.
-
-## Why Wren
-
-Wren is a small, fast, class-based, concurrent scripting language.
-
-I like the syntax much better than Lua's or Javascript's and it's a
-fairly powerful language with decent performance.
-
-## Installation
-
-This is a WIP, I may end up vendoring wren inside this project.
-
-1. Install [wren](https://wren.io/) as a library in your system.
-
-   * In Arch: `yay install wren`
-   * In other systems: [Embedding instructions](https://wren.io/getting-started)html#embed-the-vm
-
-2. Add the dependency to your `shard.yml`:
-
-   ```yaml
-   dependencies:
-     cr-wren:
-       github: ralsina/cr-wren
-   ```
-
-3. Run `shards install`
-
-## Usage
-
-This is a fully commented example:
-
-```crystal
-require "../src/wren.cr"  # Adjust as needed
+require "../src/wren.cr"
 
 vm = Wren::VM.new "myvm"
 
@@ -90,20 +54,3 @@ vm.interpret "main", %(
   // We can pass a string as argument here because cr-wren will cast it to Float64
   System.print(Math.add("1",2,3))
 ) # 2+3.5=5.5  ¨1"+2+3=6
-```
-
-## Development
-
-This is early code, not much here.
-
-## Contributing
-
-1. Fork it (<https://github.com/ralsina/cr-wren/fork>)
-2. Create your feature branch (`git checkout -b my-new-feature`)
-3. Commit your changes (`git commit -am 'Add some feature'`)
-4. Push to the branch (`git push origin my-new-feature`)
-5. Create a new Pull Request
-
-## Contributors
-
-* [Roberto Alsina](https://github.com/ralsina) - creator and maintainer
